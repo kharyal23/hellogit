38f7dbcc4b00c490f2cfc863eb3ca2ce25a1ed21
Kharyal23 <keshavkharyal23@gmail.com>
2021-09-03 14:46:54 +0530

    fisrt

---
+++ history.py
@@ -0,0 +1,101 @@
+runfile('C:/Users/keshav/untitled10.py', wdir='C:/Users/keshav')
+runfile('C:/Users/keshav/untitled11.py', wdir='C:/Users/keshav')
+
+## ---(Thu Jun  3 17:47:10 2021)---
+runcell(0, 'C:/Users/keshav/untitled1.py')
+runfile('C:/Users/keshav/untitled1.py', wdir='C:/Users/keshav')
+runfile('C:/Users/keshav/untitled11.py', wdir='C:/Users/keshav')
+11
+runfile('C:/Users/keshav/untitled11.py', wdir='C:/Users/keshav')
+runfile('C:/Users/keshav/untitled11.py', wdir='C:/Users/keshav')
+runfile('C:/Users/keshav/untitled11.py', wdir='C:/Users/keshav')
+runfile('C:/Users/keshav/untitled11.py', wdir='C:/Users/keshav')
+4
+runfile('C:/Users/keshav/untitled11.py', wdir='C:/Users/keshav')
+runfile('C:/Users/keshav/guessinggame.py', wdir='C:/Users/keshav')
+
+## ---(Fri Jun  4 09:49:17 2021)---
+runfile('C:/Users/keshav/.spyder-py3/temp.py', wdir='C:/Users/keshav/.spyder-py3')
+runfile('C:/Users/keshav/reasoning.py', wdir='C:/Users/keshav')
+runfile('C:/Users/keshav/.spyder-py3/temp.py', wdir='C:/Users/keshav/.spyder-py3')
+
+## ---(Mon Jun  7 09:55:12 2021)---
+runfile('C:/Users/keshav/reasoning.py', wdir='C:/Users/keshav')
+runfile('C:/Users/keshav/reasoning.py', wdir='C:/Users/keshav')
+10
+
+## ---(Tue Jun  8 10:49:18 2021)---
+runfile('C:/Users/keshav/Music/chapter 3.py', wdir='C:/Users/keshav/Music')
+runcell(0, 'C:/Users/keshav/Music/chapter 3.py')
+runfile('C:/Users/keshav/Music/chapter 3.py', wdir='C:/Users/keshav/Music')
+runcell(0, 'C:/Users/keshav/Music/chapter 3.py')
+runfile('C:/Users/keshav/Music/chapter 3.py', wdir='C:/Users/keshav/Music')
+
+## ---(Wed Jun  9 10:35:58 2021)---
+runfile('C:/Users/keshav/AppData/Roaming/Microsoft/Windows/Network Shortcuts/ifelse.py', wdir='C:/Users/keshav/AppData/Roaming/Microsoft/Windows/Network Shortcuts')
+runfile('C:/Users/keshav/AppData/Roaming/Microsoft/Windows/Network Shortcuts/ifelse.py', wdir='C:/Users/keshav/AppData/Roaming/Microsoft/Windows/Network Shortcuts')
+runfile('C:/Users/keshav/AppData/Roaming/Microsoft/Windows/Network Shortcuts/ifelse.py', wdir='C:/Users/keshav/AppData/Roaming/Microsoft/Windows/Network Shortcuts')
+
+## ---(Thu Jun 10 10:06:05 2021)---
+runfile('C:/Users/keshav/AppData/Roaming/Microsoft/Windows/Network Shortcuts/untitled1.py', wdir='C:/Users/keshav/AppData/Roaming/Microsoft/Windows/Network Shortcuts')
+runfile('C:/Users/keshav/AppData/Roaming/Microsoft/Windows/Network Shortcuts/untitled1.py', wdir='C:/Users/keshav/AppData/Roaming/Microsoft/Windows/Network Shortcuts')
+runfile('C:/Users/keshav/AppData/Roaming/Microsoft/Windows/Network Shortcuts/untitled1.py', wdir='C:/Users/keshav/AppData/Roaming/Microsoft/Windows/Network Shortcuts')
+runfile('C:/Users/keshav/AppData/Roaming/Microsoft/Windows/Network Shortcuts/untitled1.py', wdir='C:/Users/keshav/AppData/Roaming/Microsoft/Windows/Network Shortcuts')
+runfile('C:/Users/keshav/AppData/Roaming/Microsoft/Windows/Network Shortcuts/untitled1.py', wdir='C:/Users/keshav/AppData/Roaming/Microsoft/Windows/Network Shortcuts')
+runcell(0, 'C:/Users/keshav/AppData/Roaming/Microsoft/Windows/Network Shortcuts/untitled1.py')
+runfile('C:/Users/keshav/AppData/Roaming/Microsoft/Windows/Network Shortcuts/untitled1.py', wdir='C:/Users/keshav/AppData/Roaming/Microsoft/Windows/Network Shortcuts')
+
+## ---(Fri Jun 11 10:35:14 2021)---
+runfile('C:/Users/keshav/AppData/Roaming/Microsoft/Windows/Network Shortcuts/untitled1.py', wdir='C:/Users/keshav/AppData/Roaming/Microsoft/Windows/Network Shortcuts')
+
+## ---(Fri Jun 11 12:13:24 2021)---
+runfile('C:/Users/keshav/untitled1.py', wdir='C:/Users/keshav')
+
+## ---(Fri Jun 11 20:33:31 2021)---
+runfile('C:/Users/keshav/untitled1.py', wdir='C:/Users/keshav')
+runcell(0, 'C:/Users/keshav/untitled1.py')
+runfile('C:/Users/keshav/untitled1.py', wdir='C:/Users/keshav')
+
+## ---(Sat Jun 12 09:18:52 2021)---
+runfile('C:/Users/keshav/__init__self.py', wdir='C:/Users/keshav')
+
+## ---(Wed Jun 16 15:40:37 2021)---
+runfile('C:/Users/keshav/untitled1.py', wdir='C:/Users/keshav')
+runfile('C:/Users/keshav/json.py', wdir='C:/Users/keshav')
+
+## ---(Thu Jul  1 12:06:57 2021)---
+runcell(0, 'C:/Users/keshav/untitled1.py')
+runfile('C:/Users/keshav/questions.py', wdir='C:/Users/keshav')
+runfile('C:/Users/keshav/questions.py', wdir='C:/Users/keshav')
+runcell(0, 'C:/Users/keshav/questions.py')
+runfile('C:/Users/keshav/questions.py', wdir='C:/Users/keshav')
+
+## ---(Thu Jul  1 17:02:47 2021)---
+runfile('C:/Users/keshav/untitled1.py', wdir='C:/Users/keshav')
+
+## ---(Sat Jul  3 12:28:53 2021)---
+runfile('C:/Users/keshav/untitled1.py', wdir='C:/Users/keshav')
+runfile('C:/Users/keshav/untitled1.py', wdir='C:/Users/keshav')
+
+## ---(Mon Jul  5 10:12:47 2021)---
+runfile('C:/Users/keshav/untitled1.py', wdir='C:/Users/keshav')
+debugfile('C:/Users/keshav/untitled1.py', wdir='C:/Users/keshav')
+runfile('C:/Users/keshav/untitled1.py', wdir='C:/Users/keshav')
+debugfile('C:/Users/keshav/untitled1.py', wdir='C:/Users/keshav')
+
+## ---(Wed Jul 21 09:20:45 2021)---
+runfile('C:/Users/keshav/questions.py', wdir='C:/Users/keshav')
+runcell(0, 'C:/Users/keshav/questions.py')
+runfile('C:/Users/keshav/questions.py', wdir='C:/Users/keshav')
+
+## ---(Thu Jul 22 09:56:36 2021)---
+runfile('C:/Users/keshav/questions.py', wdir='C:/Users/keshav')
+
+## ---(Sat Jul 24 09:52:46 2021)---
+runfile('C:/Users/keshav/questions.py', wdir='C:/Users/keshav')
+
+## ---(Sat Jul 31 12:12:49 2021)---
+runfile('C:/Users/keshav/untitled1.py', wdir='C:/Users/keshav')
+
+## ---(Wed Aug 11 11:16:28 2021)---
+runfile('C:/Users/keshav/untitled11.py', wdir='C:/Users/keshav')
